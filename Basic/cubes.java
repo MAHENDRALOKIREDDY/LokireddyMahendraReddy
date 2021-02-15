d942e110b257e056d9fa57a430c720e9fdfba3ed
MAHENDRALOKIREDDY <79046796+MAHENDRALOKIREDDY@users.noreply.github.com>
2021-02-15 15:05:27 +0530

    LAB1

---
+++ Basic/cubes.java
@@ -0,0 +1,18 @@
+package Basic;
+
+public class cubes {
+
+		static int cubes(int n) {
+	    int sum = 0; 
+	    for (int x=1; x<=n; x++) {
+	        sum += x*x*x; }
+	    return sum; 
+	} 
+
+
+		public static void main(String[] args) {
+			 System.out.println(cubes(5)); 
+		}
+
+	}
+
