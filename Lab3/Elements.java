91d1a454a1c98f82f3151dc10d6c5c4145201a80
MAHENDRALOKIREDDY <79046796+MAHENDRALOKIREDDY@users.noreply.github.com>
2021-02-15 15:06:38 +0530

    LAB3

---
+++ Lab3/Elements.java
@@ -0,0 +1,27 @@
+package Lab3;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+public class Elements {
+
+	public static void main(String[] args) {
+
+		   Scanner sc = new Scanner(System.in);
+		   System.out.println("Enter length of array");
+		   int num = sc.nextInt();
+		   int a[] = new int[num];
+		   System.out.println("Enter array");
+		   for (int i = 0;i<num ;i++) {
+			   a[i] = sc.nextInt();
+		   }  
+		   int sum = 0;
+		   System.out.println("Your array");
+		   for (int i = 0;i<num;i++) {
+			   sum+=a[i];
+			   System.out.println(a[i]);
+		   }
+		   System.out.println("Sum of elements in array");
+		   System.out.println(sum);
+	   }   
+	} 
