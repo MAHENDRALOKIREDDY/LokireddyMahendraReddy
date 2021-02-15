732899787e10c607bae281269c575cfcb3db5804
MAHENDRALOKIREDDY <79046796+MAHENDRALOKIREDDY@users.noreply.github.com>
2021-02-15 15:06:02 +0530

    LAB2

---
+++ Look/Lab.java
@@ -0,0 +1,21 @@
+package Look;
+import java.util.*;
+public class Lab {
+
+	static int getSecondSmallest(int[] arr) {
+        Arrays.sort(arr);
+
+        return arr[1];
+    }
+
+    public static void main(String[] args) {
+        Scanner read = new Scanner(System.in);
+        int n = read.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = read.nextInt();
+        }
+        System.out.println(getSecondSmallest(arr));
+    }
+
+}
